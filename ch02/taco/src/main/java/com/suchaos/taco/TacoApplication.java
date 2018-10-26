8ebe5f6d944eec353069dc0c25069efd403b708b
suchao <822075222@qq.com>
2018-10-26 11:49:13 +0800

    开始学习第二章

---
+++ ch02/taco/src/main/java/com/suchaos/taco/TacoApplication.java
@@ -0,0 +1,12 @@
+package com.suchaos.taco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoApplication.class, args);
+    }
+}
